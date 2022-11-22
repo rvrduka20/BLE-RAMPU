ea3f59c649e71e7511a9b650816c4e02bc7fb785
mtomasic20 <mtomasic20@student.foi.hr>
2022-11-22 04:24:06 +0100

    Add bluetooth state check

--- HeartRateMonitor/HRMViewController.swift
+++ HeartRateMonitor/HRMViewController.swift
@@ -1,12 +1,15 @@
 
 import UIKit
+import CoreBluetooth
 
 class HRMViewController: UIViewController {
 
   @IBOutlet weak var heartRateLabel: UILabel!
   @IBOutlet weak var bodySensorLocationLabel: UILabel!
+  var centralManager: CBCentralManager!
 
   override func viewDidLoad() {
+    centralManager = CBCentralManager(delegate: self, queue: nil)
     super.viewDidLoad()
 
     // Make the digits monospaces to avoid shifting when the numbers change
@@ -17,4 +20,25 @@ class HRMViewController: UIViewController {
     heartRateLabel.text = String(heartRate)
     print("BPM: \(heartRate)")
   }
+}
+
+extension HRMViewController: CBCentralManagerDelegate{
+  func centralManagerDidUpdateState(_ central: CBCentralManager) {
+    switch central.state {
+      case .unknown:
+        print("central.state is .unknown")
+      case .resetting:
+        print("central.state is .resetting")
+      case .unsupported:
+        print("central.state is .unsupported")
+      case .unauthorized:
+        print("central.state is .unauthorized")
+      case .poweredOff:
+        print("central.state is .poweredOff")
+      case .poweredOn:
+        print("central.state is .poweredOn")
+    }
+  }
+  
+  
 }
